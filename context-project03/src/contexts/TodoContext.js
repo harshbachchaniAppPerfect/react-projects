d282dc7d00e753ef52d9242c8f7945a4955b07c6
harshbachchaniAppPerfect <harshb@AppPerfect.com>
2024-08-21 14:13:21 +0530

    add project react-router with localstorage

--- context-project03/src/contexts/TodoContext.js
+++ context-project03/src/contexts/TodoContext.js
@@ -1,19 +1,18 @@
 import { useContext, createContext } from "react";
 
-export const TodoContext = () =>
-  createContext({
-    todos: [
-      {
-        id: 1,
-        title: "My Title",
-        completed: false,
-      },
-    ],
-    addTodo: (todo) => {},
-    updateTodo: (id, todo) => {},
-    deleteTodo: (id) => {},
-    toggleComplete: (id) => {},
-  });
+export const TodoContext = createContext({
+  todos: [
+    {
+      id: 1,
+      title: "My Title",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
 
 export const TodoProvider = TodoContext.Provider;
 
